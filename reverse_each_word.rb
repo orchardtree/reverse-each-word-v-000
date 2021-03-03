d4b88da1d75a3ed60513d407e6c77488c78f65ab
Jesse Wenger <wengerj@gmail.com>
2021-03-03 12:49:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   word_reverse = []
-  sentence.split.collect do |word|
+  sentence.split.each do |word|
     word_reverse << word.reverse
   end
   word_reverse.join(" ")
